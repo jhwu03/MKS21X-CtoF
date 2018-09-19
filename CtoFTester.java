b846504c648108c41791814754f5c5a32dbc10c0
jwu03 <jwu03@stuy.edu>
2018-09-19 17:14:34 -0400

    code not tested

---
+++ CtoFTester.java
@@ -0,0 +1,11 @@
+public class CtoFTester{
+  public static double CtoF(double temp){
+    return (temp * 1.8 + 32);
+  }
+  public static double FtoC(double temp){
+    return ((temp - 32)/ 1.8);
+    public static void main(){
+      system.out.println(CtoF(100))
+      system.out.println(FtoC(100))
+    }
+}
